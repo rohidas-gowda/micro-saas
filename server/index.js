51daa80e3e6459b735780b246bc5eaf739909621
rohidasgowda@gmail.com <rohidasgowda@gmail.com>
2023-05-26 21:59:02 +0530

    middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-app.get('/hello', (req, res)=>{
-    res.send('hello world')
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('')
+
+app.post('/api/register', (req, res)=>{
+    console.log(req.body);
+    res.json({ status: 'ok'})
 })
 
 app.listen(1337, () => {
