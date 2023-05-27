02a25a3416d1a5888b272547fd29380b6ba0bf87
rohidasgowda@gmail.com <rohidasgowda@gmail.com>
2023-05-27 22:57:36 +0530

    mongo db

--- server/index.js
+++ server/index.js
@@ -2,15 +2,40 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const User = require('./models/user.model')
 
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('')
+mongoose.connect('mongodb://0.0.0.0:27017/mern-stack-login')
 
-app.post('/api/register', (req, res)=>{
+app.post('/api/register', async (req, res) => {
     console.log(req.body);
-    res.json({ status: 'ok'})
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({ status: 'ok' })
+    } catch (error) {
+        res.json({ status: 'error', error: 'Duplicate email' })
+    }
+})
+
+app.post('/api/login', async (req, res) => {
+
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password,
+    })
+
+    if(user) {
+        return res.json({ status: 'ok', user: true })
+    } else {
+        return res.json({ status: 'error', user: false})
+    }
+
 })
 
 app.listen(1337, () => {
